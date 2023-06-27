8d7514180cea4bc092e8df37e9f7c03212b0e5bd
Ola Olawoyin <82355238+Ola-Olawoyin@users.noreply.github.com>
2023-06-27 14:49:49 +0100

    Update tests.java

--- src/test/java/testcases/tests.java
+++ src/test/java/testcases/tests.java
@@ -27,9 +27,16 @@ public class tests {
 			 driver = new EdgeDriver();
 		}
 
-		driver.get("https://www.testingementor.com");
-		System.out.println(driver.getTitle());
-		driver.quit();
+		driver.get("https://www.qa.way2automation.com");
+                driver.findElements(RelativeLocator.with(By.TagName("input").above(By.tagName("select").get(2).sendKeys("Ola Olawoyin");
+		driver.findElements(RelativeLocator.with(By.TagName("input").above(By.tagName("select").get(1).sendKeys("07588859542");
+		driver.findElements(RelativeLocator.with(By.TagName("input").above(By.tagName("select").get(0).sendKeys("oola.olawoyin@gmail.com");
+		driver.findElements(RelativeLocator.with(By.TagName("input").below(By.tagName("select").get(0).sendKeys("London");
+		driver.findElements(RelativeLocator.with(By.TagName("input").above(By.tagName("select").get(1).sendKeys("Ola17");
+		driver.findElements(RelativeLocator.with(By.TagName("input").above(By.tagName("select").get(2).sendKeys("QAAuto17@");
+		driver.findElement(RelativeLocator.with(By.TagName("input").get.near(By.linkText("Signin").click();
+		//System.out.println(driver.getTitle());
+		//driver.quit();
 
 	}
 
